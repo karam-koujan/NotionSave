f7b43a8fe4ca9ee024eee1b0f52e08455bbf750f
karam-koujan <karamkaku2000@gmail.com>
2023-05-31 19:04:47 +0100

    chore:get video metadata by url

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const { Client } = require("@notionhq/client")
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const app = express();
+const youtube = require('youtube-metadata-from-url');
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors({
+  origin:"https://www.youtube.com"
+}))
+// Initializing a client
+console.log(process.env.NOTION_TOKEN)
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+})
+
+const getUsers = async () => {
+    return  await notion.users.list({})
+}
+const createPage = async (link)=>{
+    const {results} = await notion.search({
+        filter: {
+          value: 'page',
+          property: 'object'
+        }
+      });
+      const pagesId = results.map(({id})=>id)
+      if (pagesId){
+         console.log(pagesId)
+          const response = await notion.pages.create({
+            parent: {
+                type:"page_id",
+                page_id : pagesId[0]
+                
+            },
+            properties: {
+                
+                    
+                      "title": [{ "type": "text", "text": { "content": link} }]
+                    }
+                  
+            
+          })
+      }
+}
+
+
+
+
+app.post('/api/link', (req, res) => {
+  const { link,type } = req.body;
+  console.log(link)
+  youtube.metadata(link).then((json) =>{
+    console.log(json);
+  }, (err)=>{
+    console.log(err);
+  });
+
+  res.status(201).json({ message: 'Link is sent ' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
