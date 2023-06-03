5453976af089316305e939b4fbeb70b1bad4ebb2
karam-koujan <karamkaku2000@gmail.com>
2023-06-03 14:49:34 +0100

    chore: add save to notion to twitter

--- scripts/content.js
+++ scripts/content.js
@@ -49,11 +49,49 @@ const saveButton = createElement("div",{style:"display:flex;justify-content:cent
 getElementByAttr("#actions",(element)=>{
   element.insertAdjacentElement("afterbegin",saveButton) 
 })
-console.log("element")
-getElementByAttr("[data-testid='Dropdown']",(element)=>{
-  const saveText = createElement("p",{style:"color:#e7e9ea;font-family:TwitterChirp;font-size:15px;font-weight:bold;padding:15px 0 12px 3rem;margin:0;"},"Save to Notion")
-  const wrapper = createElement("div",{style:"cursor:pointer"},saveText)
-  element.insertAdjacentElement("afterbegin",wrapper) 
+
+/*
+document.addEventListener('DOMSubtreeModified', (e) => {
+      const roleMenu = e.target.getAttribute("role");
+      let isElementExist = document.getElementById("notion")
+      console.log(roleMenu)
+      console.log(!isElementExist)
+      if(roleMenu === "menu"&&!isElementExist){
+        getElementByAttr("[data-testid='Dropdown']",(element)=>{
+          console.log(element,"eleme")
+          const saveText = createElement("p",{style:"color:#e7e9ea;font-family:TwitterChirp;font-size:15px;font-weight:bold;padding:15px 0 12px 3rem;margin:0;"},"Save to Notion")
+          const wrapper = createElement("div",{id:"notion",style:"cursor:pointer"},saveText)
+          element.insertAdjacentElement("afterbegin",wrapper) })
+         isElementExist = true 
+        }
+      
+       
+})
+*/
+
+/*
+getElementByAttr("[data-testid='caret']",(element)=>{
+  const icon = element.parentElement.parentElement.parentElement.parentElement
+  const container = icon.parentElement
+  const saveSpan = createElement("span",{style:"color:#0f0f0f;font-size:14px;font-family:Roboto,Arial,sans-serif;font-weight:bold;text-transform:capitalize;"},"notion")
+  const saveButton = createElement("div",{style:"display:flex;justify-content:center;align-item:center;padding:8px 16px;cursor:pointer; background:white;border-radius:18px;transition:opacity 0.6 ease-in;",onmouseover:"this.style.opacity=0.9",onmouseout:"this.style.opacity=1"},saveSpan)
+  const flex = createElement("div",{style:"display:flex;align-items:center;"},[saveButton,icon])
+ container.appendChild(flex)
+  console.log(parent)
+})
+*/
+
+
+
+
+getElementByAttr("[data-testid='caret']",(element)=>{
+ element.addEventListener("click",()=>{
+  getElementByAttr("[data-testid='Dropdown']",(element)=>{
+    console.log(element,"eleme")
+    const saveText = createElement("p",{style:"color:#e7e9ea;font-family:TwitterChirp;font-size:15px;font-weight:bold;padding:15px 0 12px 3rem;margin:0;"},"Save to Notion")
+    const wrapper = createElement("div",{id:"notion",style:"cursor:pointer"},saveText)
+    element.insertAdjacentElement("afterbegin",wrapper) })
+ })
 })
 const getLink = () => window.location.href
 saveButton.addEventListener("click",()=>{
