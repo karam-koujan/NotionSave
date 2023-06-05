c36c2bd1f405c39300aecd11dca65accc1961bb9
karam-koujan <karamkaku2000@gmail.com>
2023-06-06 00:24:49 +0100

    fix: fix bugs during authentification

--- src/popup.js
+++ src/popup.js
@@ -31,18 +31,6 @@ const auth = () => {
       const redirectUrlQuery = getCodeAndErrorFromRedirectUri(redirectUrl);
       console.log("dd");
       if (redirectUrlQuery.code) {
-        fetch(
-          `http://localhost:3000/api/auth?code=${redirectUrlQuery.code}&error=${redirectUrlQuery.error}`
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            console.log("data", data.data);
-            localStorage.setItem("user", JSON.stringify(data.data));
-
-            if (data.error) {
-              return console.log("error");
-            }
-          });
       }
       localStorage.setItem("redirectUrlCode", JSON.stringify(redirectUrlQuery));
       console.log("token letsift");
@@ -50,34 +38,6 @@ const auth = () => {
   );
 
   const user = JSON.parse(localStorage.getItem("user"));
-  setTimeout(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { accessToken: user.access_token });
-    });
-  }, 500);
-};
-window.onload = () => {
-  const btn = document.getElementById("checkButton");
-  const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user);
-  fetch("http://localhost:3000/api/dbId", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: user.access_token,
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res.dbId.length);
-      if (res.error || res.dbId.length === 0) {
-        btn.textContent = "Create Notion Database";
-      } else {
-        btn.textContent = "database is created";
-        btn.style.opacity = "0.8";
-        btn.disabled = true;
-      }
-    });
   if (user) {
     setTimeout(() => {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -86,18 +46,126 @@ window.onload = () => {
     }, 500);
   }
 };
+window.onload = () => {
+  const btn = document.getElementById("checkButton");
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+
+  if (user) {
+    fetch("http://localhost:3000/api/dbId", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: user.access_token,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.dbId.length);
+        if (res.error || res.dbId.length === 0) {
+          btn.textContent = "Create Notion Database";
+        } else {
+          btn.textContent = "database is created";
+          btn.style.opacity = "0.8";
+          btn.disabled = true;
+        }
+      });
+    setTimeout(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { accessToken: user.access_token });
+      });
+    }, 500);
+  }
+};
 document.addEventListener("DOMContentLoaded", () => {
-  const loginBtn = document.getElementById("login");
-  const isLogged = JSON.parse(localStorage.getItem("redirectUrlCode"));
+  const loginBtn = createElement(
+    "button",
+    { id: "login", class: "createDb" },
+    "Connect to notion"
+  );
+  const loginWrapper = document.getElementById("connect");
+  const btn = document.getElementById("checkButton");
+
   loginBtn.addEventListener("click", auth);
+  const redirectUrlQuery = JSON.parse(localStorage.getItem("redirectUrlCode"));
+  const user =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : undefined;
+
+  if (!user && redirectUrlQuery) {
+    fetch(
+      `http://localhost:3000/api/auth?code=${redirectUrlQuery.code}&error=${redirectUrlQuery.error}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data.data);
+        fetch("http://localhost:3000/api/dbId", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: data.data.access_token,
+          },
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res.dbId.length);
+            if (res.error || res.dbId.length === 0) {
+              btn.textContent = "Create Notion Database";
+            } else {
+              btn.textContent = "database is created";
+              btn.style.opacity = "0.8";
+              btn.disabled = true;
+            }
+          });
+        localStorage.setItem("user", JSON.stringify(data.data));
+
+        loginBtn.parentElement.removeChild(loginBtn);
+        const nameTag = createElement(
+          "p",
+          {
+            style:
+              "color:#37352f;font-size:17px;text-align:center;font-weight:bold;margin-top:.5rem;",
+          },
+          data.data.owner.user.name
+        );
+        const profileImg = createElement(
+          "img",
+          {
+            style:
+              "object-fit:cover;border-radius:50%;width:100%;display:block;",
+            src: data.data.owner.user.avatar_url,
+            alt: data.data.owner.user.name,
+          },
+          ""
+        );
+        const imgContainer = createElement(
+          "div",
+          {
+            style:
+              "border-radius:50%; margin-inline:auto; width:100px; margin-top:1rem;",
+          },
+          profileImg
+        );
+        const parent = document.getElementById("parent");
+        parent.appendChild(imgContainer);
+        parent.appendChild(nameTag);
+        btn.style.display = "block";
+        if (data.error) {
+          return console.log("error");
+        }
+      });
+  }
+  const isLogged = JSON.parse(localStorage.getItem("user"));
   console.log("isLogged", isLogged);
-  if (isLogged !== null) {
-    loginBtn.parentElement.removeChild(loginBtn);
+  if (isLogged === null) {
+    loginWrapper.appendChild(loginBtn);
   }
 
-  const btn = document.getElementById("checkButton");
-  const user = JSON.parse(localStorage.getItem("user"));
-
+  if (!user) {
+    btn.style.display = "none";
+  }
   btn.addEventListener("click", () => {
     btn.textContent = "Creating...";
     fetch("http://localhost:3000/api/createDB", {
